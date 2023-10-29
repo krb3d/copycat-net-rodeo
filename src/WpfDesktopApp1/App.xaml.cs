4ad93f185dcfdde686a72d0bce63977a218471ff
Serhii <5120853+krb3d@users.noreply.github.com>
2023-10-29 13:50:15 +0200

    Basic UI Layout (#2)
    * Add basic UI layout

---
+++ src/WpfDesktopApp1/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+using WpfDesktopApp1.ViewModel;
+
+namespace WpfDesktopApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddScoped<IMainWindowViewModel, MainWindowViewModel>();
+                        services.AddSingleton<MainWindow>();
+                    })
+                    .Build();
+
+        using var serviceScope = _host.Services.CreateScope();
+        var services = serviceScope.ServiceProvider;
+
+        try
+        {
+            var mainWindow = services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERROR: {ex}");
+        }
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        using (_host)
+        {
+            await _host.StopAsync();
+        }
+
+        base.OnExit(e);
+    }
+}
+
---
+++ src/WpfDesktopApp1/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfDesktopApp1.ViewModel;
+
+namespace WpfDesktopApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(
+            IMainWindowViewModel context)
+        {
+            InitializeComponent();
+            DataContext = context;
+        }
+    }
+}
